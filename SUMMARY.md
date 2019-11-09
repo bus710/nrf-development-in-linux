af4739f0e60e1290fe396d34a3c874153ff62436
bus710 <bus710@gmail.com>
2019-11-09 14:51:56 -0800

    A minor update

--- SUMMARY.md
+++
@@ -1,4 +0,0 @@
-# Summary
-
-* [Installation](README.md)
-
---
+++ scripts/00_git_setup.sh
@@ -0,0 +1,5 @@
+git config remote.origin.url https://bus710@github.com/bus710/nrf-development-in-linux
+git config --global user.email "bus710@gmail.com"
+git config --global user.name "bus710"
+git config --global push.default simple
+
